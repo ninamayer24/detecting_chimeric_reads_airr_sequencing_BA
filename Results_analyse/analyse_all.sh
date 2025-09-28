5d2dbf2dac639dff4cf308eab5aba346bdd7cfdc
Nina Mayer <ni.mayer@student.uni-tuebingen.de>
2025-09-28 23:34:08 +0200

    analyse
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                #

---
+++ Results_analyse/analyse_all.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+python analyse_all.py \
+  --logs-root "/home-link/zxozk31/Analyse_cons_count/Results_vsearch_simulated6" \
+  --chmm-root "/home-link/zxozk31/Analyse_cons_count/results_chmmairra_sim6" \
+  --out "/home-link/zxozk31/Analyse_cons_count/Results_analyse/chimera_summary_all_simulated6.csv"
