dd8a48bb8ade9310fff3e3667513a7c20285e2dd
ninamayer24 <137504350+ninamayer24@users.noreply.github.com>
2025-09-28 23:53:30 +0200

    Create simulation.sh

---
+++ Results_analyse/simulation.sh
@@ -0,0 +1,5 @@
+python simulation.py \
+  /home-link/zxozk31/Analyse_cons_count/input_files_with_consensus_count_filtered \
+  --recursive \
+  --percents 0.1,0.25,0.5,1,2.5,5,7.5,10,15,20,25,30,35,40,45,50 \
+  --outdir results_simulation6
